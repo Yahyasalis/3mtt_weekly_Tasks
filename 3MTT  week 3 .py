a5dba61ffd8818339731c4b29cc429f4ad9e7410
Yahyasalis <160774791+Yahyasalis@users.noreply.github.com>
2024-02-25 16:52:46 +0100

    Basic learns in python

---
+++ 3MTT  week 3 .py
@@ -0,0 +1,26 @@
+## Creating list
+ali = ["names", 'age', 'state', 'gender', 'l.g.a']
+# displaying output
+print(ali)
+## creating variable
+tanko = 'tuwon dawa'
+# printing variable
+print(tanko)
+## creating a insert 
+ali.insert(2,'musa')
+print(ali)
+##appending a list
+ali.append ('sabr')
+print(ali)
+##removing from a list
+ali.remove('age')
+print(ali)
+#creating pop in list 
+gender_pop = ali.pop(3)
+print(gender_pop)
+## creating a copy 
+ali_copy = ali.copy()
+print(ali_copy)
+# displaying the sort
+ali_copy.sort()
+print(ali_copy)
